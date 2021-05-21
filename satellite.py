520bca4ffb4103445811af5586dfc831cdbaf4b7
mjuc <michal.jucewicz@protonmail.com>
2021-05-21 13:48:45 +0200

    Added coordinates calculation

--- satellite.py
+++ satellite.py
@@ -1,19 +1,18 @@
-import numpy as np
-from math import *
 from scipy import integrate
 
 class Satelite:
-    def Satellite(self,x,y,R,Rs,G,vx,vy):
+    def Satellite(self,x,y,R,Rs,G,M):
         self.x=x
         self.y=y
         self.R=R
         self.Rs=Rs
-        self.G=G
-        self.vx=vx
-        self.vy=vy
-        
+        self.GM=G*M
+            
+    def denominator(self):
+        return (self.x**2+self.y**2)**1.5
+    
     def calculateX(self):
-        pass
+        self.x=integrate(integrate(-1*(self.GM*self.x)/self.denominator()))
     
     def calculateY(self):
-        pass
+        self.x=integrate(integrate(-1*(self.GM*self.y)/self.denominator()))
