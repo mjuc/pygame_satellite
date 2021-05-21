bfd907951ce4efde8d4685fa178b94985ab39caf
mjuc <michal.jucewicz@protonmail.com>
2021-05-21 13:10:59 +0200

    Created empty pygame window for future visualisation. Created class for satellite model.

---
+++ satellite.py
@@ -0,0 +1,19 @@
+import numpy as np
+from math import *
+from scipy import integrate
+
+class Satelite:
+    def Satellite(self,x,y,R,Rs,G,vx,vy):
+        self.x=x
+        self.y=y
+        self.R=R
+        self.Rs=Rs
+        self.G=G
+        self.vx=vx
+        self.vy=vy
+        
+    def calculateX(self):
+        pass
+    
+    def calculateY(self):
+        pass
