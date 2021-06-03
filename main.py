5690e6301c4cf6704544fc4480fe551c036eefa7
mjuc <michal.jucewicz@protonmail.com>
2021-06-03 12:13:47 +0200

    Added slider regulating G to GUI. Added creating and refreshing of satellite object.

--- main.py
+++ main.py
@@ -2,22 +2,29 @@ import pygame
 import thorpy
 from satellite import Satelite
 
-pygame.init()
+ref_flag=False
 
-screen = pygame.display.set_mode([1000, 700])
+def set_sat_refresh():
+    ref_flag=True
+
+pygame.init()
+sat=Satelite()
+screen = pygame.display.set_mode([1200, 700])
 #menu
 slider_x = thorpy.SliderX(100, (10, 60), "X axis distance")
 slider_y = thorpy.SliderX(100, (10, 60), "Y axis distance")
 slider_r = thorpy.SliderX(50, (1,10), "Planet radius")
 slider_rs = thorpy.SliderX(10, (1,5), "Satellite radius")
 slider_m = thorpy.SliderX(160, (20,100), "Planet mass")
-sat_refresh_button = thorpy.make_button("Refresh params")#uttery useless
+slider_G = thorpy.SliderX(500, (5,10), "Gravitational acceleration")
+sat_refresh_button = thorpy.make_button("Refresh params",func=set_sat_refresh)
 quit_button = thorpy.make_button("Quit", func=thorpy.functions.quit_func)
 box = thorpy.Box(elements=[slider_x,
                            slider_y,
                            slider_r,
                            slider_rs,
                            slider_m,
+                           slider_G,
                            sat_refresh_button,
                            quit_button])
 menu = thorpy.Menu(box)
@@ -35,6 +42,12 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    if ref_flag:
+        sat.set_x(slider_x.get_value())
+        sat.set_y(slider_y.get_value())
+        sat.set_R(slider_r.get_value())
+        sat.set_Rs(slider_rs.get_value())
+        sat.set_GM(slider_m.get_value(),slider_G.get_value())
     menu.react(event)
-    print(slider_x.get_value())
+
 pygame.quit()
