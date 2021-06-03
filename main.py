6bd17ff3a97db900e551bc1dc1b227f49a1a29fc
mjuc <michal.jucewicz@protonmail.com>
2021-06-03 22:15:20 +0200

    Added graphical part of simulation. Modfied calculating of coordinates.

--- main.py
+++ main.py
@@ -1,23 +1,53 @@
 import pygame
+from pygame.constants import USEREVENT
 import thorpy
 from satellite import Satelite
+import numpy as np
 
-ref_flag=False
+RUN_SIM = 1
 
-def set_sat_refresh():
-    ref_flag=True
+#defines for colours used by simulation
+WHITE = (255, 255, 255)
+BLUE = (  0,   0, 255)
+GREEN = (  0, 255,   0)
+RED = (255,   0,   0)
 
-pygame.init()
+#instance of model
 sat=Satelite()
+
+#custom pygame event responsible for starting simulation
+run_sim_event = pygame.event.Event(pygame.USEREVENT+1)
+
+#update model params
+def set_sat_refresh():
+    print("Refresh values")
+    sat.set_R(slider_r.get_value())
+    sat.set_Rs(slider_rs.get_value())
+    sat.set_GM(slider_m.get_value(),slider_G.get_value())
+    sat.set_x(slider_x.get_value())
+    sat.set_y(slider_y.get_value()) 
+
+#initialize simulation
+def run_sim():
+    print("Running simulation")
+    pygame.event.post(run_sim_event)
+    
+pygame.init()
+pygame.display.set_caption("Satellite sim")
 screen = pygame.display.set_mode([1200, 700])
+screen.fill(WHITE)
+
 #menu
+#sliders regulating model params
 slider_x = thorpy.SliderX(100, (10, 60), "X axis distance")
 slider_y = thorpy.SliderX(100, (10, 60), "Y axis distance")
 slider_r = thorpy.SliderX(50, (1,10), "Planet radius")
 slider_rs = thorpy.SliderX(10, (1,5), "Satellite radius")
 slider_m = thorpy.SliderX(160, (20,100), "Planet mass")
 slider_G = thorpy.SliderX(500, (5,10), "Gravitational acceleration")
+#buttons
 sat_refresh_button = thorpy.make_button("Refresh params",func=set_sat_refresh)
+run_sim_button = thorpy.make_button("Run simulation",func=run_sim)
 quit_button = thorpy.make_button("Quit", func=thorpy.functions.quit_func)
 box = thorpy.Box(elements=[slider_x,
                            slider_y,
@@ -26,6 +56,7 @@ box = thorpy.Box(elements=[slider_x,
                            slider_m,
                            slider_G,
                            sat_refresh_button,
+                           run_sim_button,
                            quit_button])
 menu = thorpy.Menu(box)
 
@@ -35,19 +66,42 @@ for element in menu.get_population():
 box.set_topleft((0,0))
 box.blit()
 box.update()
+pygame.display.update()
 
 running = True
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    if ref_flag:
-        sat.set_x(slider_x.get_value())
-        sat.set_y(slider_y.get_value())
-        sat.set_R(slider_r.get_value())
-        sat.set_Rs(slider_rs.get_value())
-        sat.set_GM(slider_m.get_value(),slider_G.get_value())
-    menu.react(event)
+        elif event.type == pygame.USEREVENT+1:
+            radius=sat.get_R()
+            dist_x=sat.get_x()
+            dist_y=sat.get_y()
+            sat_radius=sat.get_Rs()
+            screen.fill(WHITE)
+            box.blit()
+            box.update()
+            pygame.draw.circle(screen,BLUE,(600,350),radius)
+            pygame.draw.circle(screen,
+                               GREEN,
+                               (600+dist_x+radius+sat_radius,350+dist_y+radius+sat_radius)
+                               ,sat_radius)
+            pygame.display.update()
+            time=np.arange(0,1000,0.01)
+            X=sat.calculateX(time)
+            Y=sat.calculateY(time)
+            radius=sat.get_R()
+            sat_radius=sat.get_Rs()
+            print(len(X))
+            for i in range(0,len(X)):
+                screen.fill(WHITE)
+                box.blit()
+                box.update()
+                pygame.draw.circle(screen,BLUE,(600,350),radius)
+                pygame.draw.circle(screen,GREEN,(600+int(X[i])+radius+sat_radius,350+int(Y[i])+radius+sat_radius),sat_radius)
+                pygame.display.update()                              
+        
+        menu.react(event)
+        pygame.display.update()
 
 pygame.quit()
--- satellite.py
+++ satellite.py
@@ -1,6 +1,20 @@
-from scipy import integrate
+from scipy.integrate import odeint
 
+def denominator(x,y):
+        return (x**2+y**2)**1.5
+
+def fx(x,y,GM,ph):
+        return(-1*(x)*GM/denominator(x,y))
+
+def fy(x,y,GM,ph):
+    return(-1*(GM*y)/denominator(x,y))
 class Satelite:
+    x=0
+    y=0
+    R=1
+    Rs=1
+    GM=5
+    
     def Satellite(self,x,y,R,Rs,G,M):
         self.x=x
         self.y=y
@@ -8,20 +22,17 @@ class Satelite:
         self.Rs=Rs
         self.GM=G*M
             
-    def denominator(self):
-        return (self.x**2+self.y**2)**1.5
-    
-    def calculateX(self):
-        self.x=integrate(integrate(-1*(self.GM*self.x)/self.denominator()))
-    
-    def calculateY(self):
-        self.x=integrate(integrate(-1*(self.GM*self.y)/self.denominator()))
+    def calculateX(self,time):
+        return odeint(fx,self.y,time,(self.GM,0))
+        
+    def calculateY(self,time):
+        return odeint(fy,self.x,time,(self.GM,0))
         
     def set_x(self,x):
-        self.x=x
+        self.x=x+self.R+self.Rs
     
     def set_y(self,y):
-        self.y=y
+        self.y=y+self.R+self.Rs
         
     def set_R(self,R):
         self.R=R
@@ -31,3 +42,18 @@ class Satelite:
         
     def set_GM(self,G,M):
         self.GM=G*M
+        
+    def get_x(self):
+        return self.x
+    
+    def get_y(self):
+        return self.y
+    
+    def get_R(self):
+        return self.R
+    
+    def get_Rs(self):
+        return self.Rs
+    
+    def get_GM(self):
+        return self.GM
